92624fc094403442404671a2039b6db1783818e6
Mukesh Sharma <mukeshsharma24@gmail.com>
2025-03-04 10:54:42 +1300

    Added flow logs

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -16,7 +16,11 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  enable_flow_log = true
+  enable_flow_log                      = true
+  create_flow_log_cloudwatch_iam_role  = true
+  create_flow_log_cloudwatch_log_group = true
+  flow_log_max_aggregation_interval    = 60
+
 
   # Tags for all resources
   tags = {
